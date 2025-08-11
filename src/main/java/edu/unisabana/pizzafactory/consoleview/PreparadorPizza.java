84b6c39d646d4a121dca39b9f9c007d715ad39ed
Santiago Gutiérrez <157374921+santiagoguti17@users.noreply.github.com>
2025-08-10 23:57:01 -0500

    bafg

--- src/main/java/edu/unisabana/pizzafactory/consoleview/PreparadorPizza.java
+++ src/main/java/edu/unisabana/pizzafactory/consoleview/PreparadorPizza.java
@@ -7,8 +7,23 @@ import edu.unisabana.pizzafactory.model.Interfaces.FabricaPizza;
 import edu.unisabana.pizzafactory.model.Interfaces.Amasador;
 import edu.unisabana.pizzafactory.model.Interfaces.Horneador;
 import edu.unisabana.pizzafactory.model.Interfaces.Moldeador;
+import edu.unisabana.pizzafactory.model.Fabricas.FabricaPI;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class PreparadorPizza {
+
+    public static void main(String args[]){
+        try {
+            Ingrediente[] ingredientes=new Ingrediente[]{new Ingrediente("queso",1),new Ingrediente("jamon",2)};            
+            PreparadorPizza pp=new PreparadorPizza();
+            FabricaPizza fabrica=new FabricaPI();
+            pp.prepararPizza(fabrica,ingredientes, Tamano.MEDIANO);
+        } catch (ExcepcionParametrosInvalidos ex) {
+            Logger.getLogger(PreparadorPizza.class.getName()).log(Level.SEVERE, "Algo ha salido mal en la preparacion de la Pizza", ex);
+        }
+                
+    }
 
     public void prepararPizza(FabricaPizza fabrica, Ingrediente[] ingredientes, Tamano tam)
             throws ExcepcionParametrosInvalidos {
